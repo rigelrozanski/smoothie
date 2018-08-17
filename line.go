b4fdd12d7a38f9a17e26b44f7619b6642d22be0b
rigelrozanski <rigel.rozanski@gmail.com>
2018-08-16 21:51:04 -0400

    lines pretty swell

--- line.go
+++ line.go
@@ -6,29 +6,29 @@ import (
 
 // a classic "point"
 type Point struct {
-	X, Y big.Float
+	X, Y *big.Float
 }
 
 // boring ol' straight line
 type Line struct {
 	Start, End Point
-	M, B       big.Float //  from y = mx +b
+	M, B       *big.Float //  from y = mx +b
 }
 
 func NewLine(start, end Point) Line {
 
-	M = new(big.Float).Quo(
+	m := new(big.Float).Quo(
 		new(big.Float).Sub(end.Y, start.Y),
 		new(big.Float).Sub(end.X, start.X))
 
-	B = new(big.Float).Sub(start.Y,
-		new(big.Float).Mul(M, start.X))
+	b := new(big.Float).Sub(start.Y,
+		new(big.Float).Mul(m, start.X))
 
-	return Line{start, end, M, B}
+	return Line{start, end, m, b}
 }
 
 // min of two floats
-func Min(f1, f2 *big.Float) {
+func Min(f1, f2 *big.Float) *big.Float {
 	if f1.Cmp(f2) < 0 {
 		return f1
 	}
@@ -36,19 +36,32 @@ func Min(f1, f2 *big.Float) {
 }
 
 // min of two floats
-func Max(f1, f2 *big.Float) {
+func Max(f1, f2 *big.Float) *big.Float {
 	if f1.Cmp(f2) > 0 {
 		return f1
 	}
 	return f2
 }
 
+//_______________________________________________________________________
+
+// length of a line!
+// sqrt((x2 - x1)^2 + (y2 - y1)^2)
+func (l Line) Length() *big.Float {
+	inter1 := new(big.Float).Sub(l.End.X, l.Start.X)
+	inter2 := new(big.Float).Sub(l.End.Y, l.Start.Y)
+	inter3 := inter1.Mul(inter1, inter1)
+	inter4 := inter2.Mul(inter2, inter2)
+	inter5 := inter1.Add(inter3, inter4)
+	return inter1.Sqrt(inter5)
+}
+
 // line l will intercept line l2
 func (l Line) WillIntercept(l2 Line) bool {
-	if (Min(l.Start.X, l.End.X) > Max(l2.Start.X, l2.End.X) ||
-		Min(l2.Start.X, l2.End.X) > Max(l.Start.X, l.End.X)) &&
-		(Min(l.Start.Y, l.End.Y) > Max(l2.Start.Y, l2.End.Y) ||
-			Min(l2.Start.Y, l2.End.Y) > Max(l.Start.Y, l.End.Y)) {
+	if (Min(l.Start.X, l.End.X).Cmp(Max(l2.Start.X, l2.End.X)) > 0 ||
+		Min(l2.Start.X, l2.End.X).Cmp(Max(l.Start.X, l.End.X)) > 0) &&
+		(Min(l.Start.Y, l.End.Y).Cmp(Max(l2.Start.Y, l2.End.Y)) > 0 ||
+			Min(l2.Start.Y, l2.End.Y).Cmp(Max(l.Start.Y, l.End.Y)) > 0) {
 		return true
 	}
 	return false
@@ -57,10 +70,11 @@ func (l Line) WillIntercept(l2 Line) bool {
 // point at which two lines intercept
 func (l Line) Intercept(l2 Line) Point {
 	//  y  = (b2 m1 - b1 m2)/(m1 - m2)
-	inter1 := new(*big.Float).Mul(l2.B, l.M)
-	inter2 := new(*big.Float).Mul(l.B, l2.M)
+	inter1 := new(big.Float).Mul(l2.B, l.M)
+	inter2 := new(big.Float).Mul(l.B, l2.M)
 	inter3 := inter1.Sub(inter1, inter2)
 	y := inter3.Quo(inter3, inter1.Sub(l.M, l2.M))
-	x = new(*big.Float).Sub(y, l.B).Quo(l.M)
+	inter4 := new(big.Float).Sub(y, l.B)
+	x := inter4.Quo(inter4, l.M)
 	return Point{x, y}
 }
--- main.go
+++ main.go
@@ -5,33 +5,57 @@ import (
 	"math/big"
 )
 
-// nolint
-type (
-	NPolygon int64 // sides of the polygon
-	NSide    int64 // which side on a polyon
-)
-
 var one = big.NewFloat(1)
+
+// bounds for the calculation
+var (
+	XBoundMin = big.NewFloat(0)
+	XBoundMax = one
+)
 
 // evaluation function
 func Fn(x *big.Float) (y *big.Float) {
-	return y.Sqrt(new(*big.Float).Sub(one, new(*big.Float).Pow(x, 2)))
+	inter1 := new(big.Float).Mul(x, x)
+	inter2 := inter1.Sub(one, inter1)
+	inter3 := inter2.Sqrt(inter2)
+	return inter3
+}
+
+func lenLines(lines map[int64]Line) *big.Float {
+	totalLen := big.NewFloat(0)
+	for _, line := range lines {
+		fmt.Printf("debug line: %v\n", line)
+		fmt.Printf("debug line.Length: %v\n", line.Length())
+		totalLen = totalLen.Add(totalLen, line.Length())
+	}
+	return totalLen
 }
 
 func main() {
 
-	var n int64 = 8                     // starting number of sides
-	maxN := 2*n - 1                     // maximum number of sides for boring polygons
-	coolPolygon := make(map[NSide]Line) // cool polygon to construct out of boring ones
+	var n int64 = 6 // starting number of divisions
+	maxN := n       //2*n - 1 // maximum number of sides for boring polygons
+	//coolPolygon := make(map[int64]Line) // cool polygon to construct out of boring ones
+
+	boringPolygons := make(map[int64]map[int64]Line) // index 1: number of lines in element, index 2: element line no.
 
 	// init boring polygons
-	for i := n; i <= maxN; i++ {
-		boringPolygons := make(map[NPolygon]map[NSide]Line)
-		boringPolygons[i] = make(map[NSide]Line)
+	for i := int64(n); i <= maxN; i++ {
+		boringPolygons[i] = make(map[int64]Line)
+
+		startPoint := Point{big.NewFloat(0), big.NewFloat(1)}            // top of the circle
+		width := new(big.Float).Quo(XBoundMax, big.NewFloat(float64(i))) // width of all these pieces
+
+		for side := int64(0); side < i; side++ {
+			x2 := new(big.Float).Add(startPoint.X, width)
+			y2 := Fn(x2)
+			endPoint := Point{x2, y2}
+			boringPolygons[i][side] = NewLine(startPoint, endPoint)
+			startPoint = endPoint
+		}
+
+		fmt.Printf("line %v, length %v \n", i, lenLines(boringPolygons[i]))
 	}
 
-	// first create a group of all the boring polygons
-
-	lines[1][2] = "hoot"
-	fmt.Println(lines[1][2])
+	fmt.Println(boringPolygons)
 }
